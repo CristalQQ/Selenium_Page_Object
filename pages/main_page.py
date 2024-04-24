c0acd40bdf3b33f11016d9a4fa43f09411345ee2
Vladislav Kobzar <kobzarvllv@gmail.com>
2024-04-24 10:41:22 +0300

    Add a test case for checking the login link
    Test case:
    1. Open the main page
    2. Check that there is a link that leads to the login

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,3 +5,6 @@ from selenium.webdriver.common.by import By
 class MainPage(BasePage):
     def go_to_login_page(self):
         self.driver.find_element(By.XPATH, "//a[@id='login_link']").click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.XPATH, "//a[@id='login_link']"), "Login link is not presented"
