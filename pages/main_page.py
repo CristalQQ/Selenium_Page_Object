3d742512b65ac36c214a1be1109190387dc31049
Vladislav Kobzar <kobzarvllv@gmail.com>
2024-04-25 09:26:33 +0300

    Add verification of the transition to the login page
    The test implements a url check for the content of login,
    login and registration forms

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,12 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         self.driver.find_element(*MainPageLocators.LOGIN_LINK).click()
+        return LoginPage(driver=self.driver, url=self.driver.current_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -6,4 +6,5 @@ def test_guest_can_go_to_login_page(driver):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(driver, link)
     page.open()
-    page.should_be_login_link()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
