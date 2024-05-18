b9ce3ba2185f9c8062e15e4ee7092b638e13a7eb
Vladislav Kobzar <kobzarvllv@gmail.com>
2024-05-18 16:17:17 +0300

    add verification that the user has logged in

--- pages/base_page.py
+++ pages/base_page.py
@@ -32,6 +32,10 @@ class BasePage:
         assert self.is_element_present(
             *BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+            " probably unauthorised user"
+
     # The element does not appear on the page for the specified time
     def is_not_element_present(self, how, what, timeout=4):
         try:
